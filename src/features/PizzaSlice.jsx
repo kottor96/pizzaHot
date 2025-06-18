c2b8f6673ea089a09ed27ee1606e5dbea4795d0f
kottor <gardien243@gmail.com>
2025-06-18 15:08:57 +0200

    base

--- src/features/PizzaSlice.jsx
+++ src/features/PizzaSlice.jsx
@@ -1,12 +1,17 @@
 import {createSlice} from '@reduxjs/toolkit'
-import data from "../data/package.json";
+import data from "../data/pizzas.json";
+
+const coupon = [{ code:"XB2-22", value:10, type:"reduction" }, { code:"XB2-23", value:15, type:"reduction" }, { code:"42", value:1 ,type:"gratos", condition:2}]
+const initialState = { listPizza: data, pannier: [], coupon }
 
 const PizzaSlice = createSlice({
     name:"gestion pizza",
-    initialState:'',
+    initialState,
     reducers:{
-        
+        ajouterPanier(state,action){
+            const pizza = state.listPizza.find((el)=>el.nom)
+        }
     }
 })
-
+export const {ajouterPanier} = PizzaSlice.actions
 export const PizzaReducer = PizzaSlice.reducer
