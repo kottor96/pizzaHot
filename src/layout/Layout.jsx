dbdeaf40da10123be844057400a426fa924dfef7
kottor <gardien243@gmail.com>
2025-06-18 12:27:23 +0200

    nav

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 export default function Layout(params) {
     return(
         <div id="layout">
+            <Navbar/>
             <Outlet/>
         </div>
     )
