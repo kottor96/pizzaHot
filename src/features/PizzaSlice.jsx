b3e6037e78ff79b4d77058e40935741358456b38
kottor <gardien243@gmail.com>
2025-06-18 16:35:42 +0200

    fonction de base

--- src/features/PizzaSlice.jsx
+++ src/features/PizzaSlice.jsx
@@ -2,14 +2,23 @@ import {createSlice} from '@reduxjs/toolkit'
 import data from "../data/pizzas.json";
 
 const coupon = [{ code:"XB2-22", value:10, type:"reduction" }, { code:"XB2-23", value:15, type:"reduction" }, { code:"42", value:1 ,type:"gratos", condition:2}]
-const initialState = { listPizza: data, pannier: [], coupon }
+const initialState = { listPizza: data, panier: [], coupon }
 
 const PizzaSlice = createSlice({
     name:"gestion pizza",
     initialState,
     reducers:{
         ajouterPanier(state,action){
-            const pizza = state.listPizza.find((el)=>el.nom)
+            const pizza = state.listPizza.find((el)=>el.nom===action.payload)
+            state.panier.push({...pizza,ingredients:pizza.ingredients.map(el=>({...el,quantiter:true}))})
+        },
+        retirerPanier(state,action){
+            state.panier=state.panier.filter((el,i)=> i === action.payload)
+        },
+        switchIngredient(state,action){
+            const pizzaBis = state.panier[action.payload.index]
+            const ingredient = pizzaBis[action.payload.index]
+            ingredient.quantiter=!ingredient.quantiter
         }
     }
 })
