b3e24b43d8c9698512e25bfcc17b33926170419d
kottor <gardien243@gmail.com>
2025-06-18 14:30:35 +0200

    nav fini

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,27 +12,29 @@ export default function Navbar(params) {
                 <div className="navbar-collapse" id="navbarText">
                     <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <NavLink className="nav-link disabled">Home</NavLink>
+                            <a className="nav-link disabled">Home</a>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link disabled">Entrées</NavLink>
+                            <a className="nav-link disabled">Entrées</a>
                         </li>
                         <li className="nav-item">
                             <NavLink to={'/'} className="nav-link active">Pizza</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link disabled">Spécialités</NavLink>
+                            <a className="nav-link disabled">Spécialités</a>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link disabled">Boissons</NavLink>
+                            <a className="nav-link disabled">Boissons</a>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link disabled">Desserts</NavLink>
+                            <a className="nav-link disabled">Desserts</a>
                         </li>
                     </ul>
                     <div className="d-flex gap-2">
-                        <a>Se connecter</a>
-                        <a>fr<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-360 280-560h400L480-360Z"/></svg></a>
+                        <ul className="navbar-nav">
+                            <a className="nav-link text-light" id="connect">Se connecter</a>
+                            <a className="nav-link text-light">fr<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-360 280-560h400L480-360Z"/></svg></a>
+                        </ul>
                     </div>
                 </div>
             </div>
