1dd6b01d5cb7e78461adb8e40ddda77c1b4310ea
kadirdolek <kadirdolek@outlook.com>
2025-06-18 16:36:30 +0200

    css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(){
 
 
     return <>
-    <footer style={{backgroundColor:'rgb(35, 31, 32)', height:'50vh' ,padding:'5% 15% 0 15%',}}>
+    <footer style={{backgroundColor:'rgb(35, 31, 32)', height:'auto' ,padding:'5% 15% 0 15%',}}>
         <div style={{width:'100%', display:'flex', justifyContent:'center', textAlign:'center'}}> 
             <div style={{display:'flex', flexDirection:'column', width:'33%'}}>
                 <h6 style={{color:'white', marginBottom:'10%'}}>A propos de Pizza Hut</h6>
