5d7b8faabc3971a0dd3fa07b648a7ecd335a0a13
kadirdolek <kadirdolek@outlook.com>
2025-06-18 14:44:46 +0200

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
-import Footer from "../components/Footer"
 
 
 export default function Home(params) {
     return
 
     <>
-        <div style={{width:'100%'}}>
+        <div style={{width:'100%', height:'60vh'}}>
             <div style={{width:'66%'}}>
 
             </div>
-            <div>
+            <div style={{width:'33%'}}>
 
             </div>
         </div>
