e0feb7e8efc45f1d79aa4f52081bea4a703a8866
kadirdolek <kadirdolek@outlook.com>
2025-06-18 15:12:03 +0200

    map home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,26 @@
+import { useSelector } from "react-redux";
 
 
-export default function Home(params) {
-    return
+export default function Home() {
+
+const listPizza = useSelector((state)=> state.pizza.listPizza)
+
+    return(
 
     <>
-        <div style={{width:'100%', height:'60vh'}}>
-            <div style={{width:'66%'}}>
-
+        <div style={{width:'100%', height:'80vh', padding:'0 10% 0 10%', display:'flex', backgroundColor:'rgb(245, 247, 249)'}}>
+            <div style={{width:'66%', backgroundColor:'wheat', height:'100%'}}>
+                {listPizza.map((element,index)=>
+                    <li key={index}>
+                        {element.name}
+                    </li>
+                )}
             </div>
-            <div style={{width:'33%'}}>
+            <div style={{width:'33%', height:'100%', display:'flex',padding:'2% 0 0 2%'}}>
+                <div style={{backgroundColor:'white', height: '600px', width:'370px'}}>
 
+                </div>
             </div>
         </div>
-    </>
+    </>)
 }
