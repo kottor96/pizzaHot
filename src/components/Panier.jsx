819a48582172f3d462aa3b4386be4d527cc4895e
kadirdolek <kadirdolek@outlook.com>
2025-06-19 11:18:51 +0200

    ajout ternaire

--- src/components/Panier.jsx
+++ src/components/Panier.jsx
@@ -1,24 +1,27 @@
+import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-
-
-
+import { retirerPanier, toggleModifier } from "../features/pizzaSlice" 
 
 export default function Panier(){
 
 const panier = useSelector((state)=> state.pizza.panier)
+const [isModifPizzaVisible, setIsModifPizzaVisible] = useState(true) 
 const dispatch = useDispatch()
 
     return(
         <>
             <div style={{backgroundColor:'white', height: '600px', width:'370px', position:'fixed'}}>
-                    {panier.map((el,index)=>(
-                        <div>
+                    {panier.map((el,index)=>
+                        {!isModifPizzaVisible ? (<div className="btnPanier" key={index}>
                             <p>{el.name}</p>
                             <button onClick={()=>dispatch(retirerPanier(index))}>supprimer</button>
                             <button onClick={()=>dispatch(toggleModifier(index))}>modifier</button>
-                        </div>
-                    ))}
+                        </div>) : null}
+                    )}
                 </div>
+              
+
+
         </>
     )
 }
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 
-export default function Layout(params) {
+export default function Layout() {
     return(
         <div id="layout">
             <Navbar/>
