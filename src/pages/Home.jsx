f99854cf61c00e16e6bd70ce7d489f0be372b11d
kadirdolek <kadirdolek@outlook.com>
2025-06-20 14:57:03 +0200

    panier

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,127 @@
-import { useSelector,useDispatch } from "react-redux";
-import  './Home.css'
+import { useSelector, useDispatch } from "react-redux";
+import "./Home.css";
 import { ajouterPanier } from "../features/pizzaSlice";
 import Panier from "../components/Panier";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export default function Home() {
-    const navigate = useNavigate()
-    const listPizza = useSelector((state)=> state.pizza.listPizza)
-    const dispatch = useDispatch()
-    const panier = useSelector((state) => state.pizza.panier);
+  const navigate = useNavigate();
+  const listPizza = useSelector((state) => state.pizza.listPizza);
+  const dispatch = useDispatch();
+  const panier = useSelector((state) => state.pizza.panier);
 
-    
-    
-    
-    
-    return(
-
+  return (
     <>
-        <div style={{width:'100%', height:'auto', padding:'0 10% 0 10%', display:'flex', backgroundColor:'rgb(245, 247, 249)', paddingBottom:'10%'}}>
-            <div style={{width:'66%', backgroundColor:'rgb(245, 247, 249)', height:'100%', display:'flex', flexWrap:'wrap', marginTop:'5.9%'}}>
-                {listPizza.map((element, index) =>
-                    <div id="pizzaCard" style={{display:'flex', flexDirection:'column', gap:'3%', margin:'0 auto 30px auto', backgroundColor:'white', width:'260px', position: 'relative', borderRadius:'5%'}} key={index}>
-                        <div className="pizza-image-container" style={{position: 'relative', width: '260px', height: '200px'}}>
-                          <img style={{height:'200px', width:'260px', marginBottom:'0', display: 'block', borderTopLeftRadius:'5%', borderTopRightRadius:'5%'}} src={element.image} alt="" />
-                          <div style={{display:'flex'}} className="pizza-ingredients-overlay">
-                            {element.ingredients.map((ingredient, idx) =>
-                              <span style={{margin:'0', fontSize:'13px'}} key={idx}>{ingredient.name} <br /></span> 
-                            )}
-                          </div>
-                        </div>
-                        <p style={{margin:'0'}}><strong> {element.name}</strong></p><br />
+      <div
+        style={{
+          width: "100%",
+          height: "auto",
+          padding: "0 10% 0 10%",
+          display: "flex",
+          backgroundColor: "rgb(245, 247, 249)",
+          paddingBottom: "10%",
+        }}
+      >
+        <div
+          style={{
+            width: "66%",
+            backgroundColor: "rgb(245, 247, 249)",
+            height: "100%",
+            display: "flex",
+            flexWrap: "wrap",
+            marginTop: "5.9%",
+          }}
+        >
+          {listPizza.map((element, index) => (
+            <div
+              id="pizzaCard"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "3%",
+                margin: "0 auto 30px auto",
+                backgroundColor: "white",
+                width: "260px",
+                position: "relative",
+                borderRadius: "5%",
+              }}
+              key={index}
+            >
+              <div
+                className="pizza-image-container"
+                style={{
+                  position: "relative",
+                  width: "260px",
+                  height: "200px",
+                }}
+              >
+                <img
+                  style={{
+                    height: "200px",
+                    width: "260px",
+                    marginBottom: "0",
+                    display: "block",
+                    borderTopLeftRadius: "5%",
+                    borderTopRightRadius: "5%",
+                  }}
+                  src={element.image}
+                  alt=""
+                />
+                <div
+                  style={{ display: "flex" }}
+                  className="pizza-ingredients-overlay"
+                >
+                  {element.ingredients.map((ingredient, idx) => (
+                    <span style={{ margin: "0", fontSize: "13px" }} key={idx}>
+                      {ingredient.name} <br />
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <p style={{ margin: "0" }}>
+                <strong> {element.name}</strong>
+              </p>
+              <br />
 
-                        <p style={{marginLeft:'auto', marginRight:'3%'}}>à partir de 
-                            <strong> €{element.price} </strong> 
-                            <button onClick={()=>{dispatch(ajouterPanier(index)),navigate(`/modif/${panier.length}`)}} style={{backgroundColor:'rgb(199, 1, 26)', color:'white', border:'none', height:'40px', width:'40px', borderRadius:'10px'}} >+</button>
-                        </p>
-                    </div>
-                )}
+              <p style={{ marginLeft: "auto", marginRight: "3%" }}>
+                à partir de
+                <strong> €{element.price} </strong>
+                <button
+                  onClick={() => {
+                    dispatch(ajouterPanier(index)),
+                      navigate(`/modif/${panier.length}`);
+                  }}
+                  style={{
+                    backgroundColor: "rgb(199, 1, 26)",
+                    color: "white",
+                    border: "none",
+                    height: "40px",
+                    width: "40px",
+                    borderRadius: "10px",
+                  }}
+                >
+                  +
+                </button>
+              </p>
             </div>
-            <div style={{width:'33%', height:'100%', display:'flex',padding:'2% 0 0 2%', marginTop:'3.9%'}}>
-                <Panier/>
-            </div>
+          ))}
         </div>
-    </>)
+        <div
+          style={{
+            position: "sticky",
+            top: "6%",
+            width: "33%",
+            height: "100%",
+            display: "flex",
+            padding: "2% 0 0 2%",
+            marginTop: "3.9%",
+            marginBottom: "30px",
+          }}
+        >
+          <Panier />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/ModifPizza.jsx
+++ src/pages/ModifPizza.jsx
@@ -1,53 +1,96 @@
 import { useEffect } from "react";
-import { useSelector,useDispatch } from "react-redux";
-import { useParams,useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
 import Panier from "../components/Panier";
 import { cancel, switchIngredient, valider } from "../features/pizzaSlice";
-import '../App.css'
+import "../App.css";
 
+export default function ModifPizza() {
+  const panier = useSelector((state) => state.pizza.panier);
 
+  const { recup } = useParams();
+  const indexPage = parseInt(recup);
+  const dispatch = useDispatch();
+  const pizzaBis = useSelector((state) => state.pizza.modifierPizza);
+  const pizza = pizzaBis ?? panier[indexPage];
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!pizza) {
+      navigate("/");
+    }
+  }, [pizza]);
 
-export default function ModifPizza(){
-    const panier = useSelector((state)=>state.pizza.panier)
-    
-    const {recup} = useParams()
-    const indexPage = parseInt(recup);
-    const dispatch = useDispatch()
-    const pizzaBis = useSelector((state)=>(state.pizza.modifierPizza))
-    const pizza = pizzaBis ?? panier[indexPage]
-    const navigate = useNavigate()
-    useEffect(()=>{
-        if(!pizza){
-            navigate('/')
-        }
-    },[pizza])
-     
-    return(
-        <section>
-            <div id="modifPizza">
-                <div id="modif1">
-                    <button onClick={()=>{dispatch(cancel()),navigate(-1)}}>retour</button>
-                    {pizza
-                    ?   <div>
-                            <div><img src={pizza.image} alt={pizza.name} /></div>
-                            <div>
-                                {pizza.ingredients.map((el,index)=>(
-                                    <div key={index}>
-                                        <p>{el.name}</p>
-                                        <button onClick={()=>{dispatch(switchIngredient({index,indexBis:indexPage}))}}>{el.quantiter?"Retirer":"Annuler"}</button>
-                                    </div>
-                                ))}
-                            </div>
-                            <button onClick={()=>{dispatch(valider(indexPage)),navigate('/')}}>valider</button>
-                        </div>
-                    :
-                    null
-                    }
-                </div>
-                <div id="modif2">
-                    <Panier/>
-                </div>
+  return (
+    <section>
+      <div id="modifPizza">
+        <div id="modif1">
+          <div>
+            <button
+              className="retour"
+              onClick={() => {
+                dispatch(cancel()), navigate(-1);
+              }}
+            >
+              Retour
+            </button>
+          </div>
+          {pizza ? (
+            <div>
+              <div>
+                <img
+                  style={{ borderRadius: "5px", marginBottom: "5%" }}
+                  src={pizza.image}
+                  alt={pizza.name}
+                />
+              </div>
+              <div>
+                <p
+                  style={{
+                    fontSize: "20px",
+                    color: "rgb(200, 16, 46)",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Ingrédients:
+                </p>
+                {pizza.ingredients.map((el, index) => (
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      padding: "0 12.5% 0 12.5%",
+                    }}
+                    key={index}
+                  >
+                    <p style={{ fontWeight: "bold" }}>{el.name}</p>
+                    <button
+                      id="RetireIng"
+                      onClick={() => {
+                        dispatch(
+                          switchIngredient({ index, indexBis: indexPage })
+                        );
+                      }}
+                    >
+                      {el.quantiter ? "Retirer" : "Annuler"}
+                    </button>
+                  </div>
+                ))}
+              </div>
+              <button
+                id="valid"
+                onClick={() => {
+                  dispatch(valider(indexPage)), navigate("/");
+                }}
+              >
+                Valider
+              </button>
             </div>
-        </section>
-    )
+          ) : null}
+        </div>
+        <div id="modif2">
+          <Panier />
+        </div>
+      </div>
+    </section>
+  );
 }
